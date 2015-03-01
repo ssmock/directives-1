273b85bd247aa126b4bf74c62b33aa97578093cd
ssmock <ssmock@gmail.com>
2015-03-01 16:15:02 -0500

    Added d-check-glyph

--- app.js
+++ app.js
@@ -7,10 +7,38 @@ app.controller("main", main);
 function main() {
     var vm = this;
 
-    this.Left = "here";
-    this.Right = "there";
-    this.Input1 = "";
-    this.Things = [];
+    vm.Left = "here";
+    vm.Right = "there";
+    vm.Input1 = "";
+    vm.Things = [];
+    vm.Check1 = {
+        Ok: false,
+        Bad: false,
+        OkIt: okIt,
+        BadIt: badIt,
+        CancelIt: cancelIt,
+        BothIt: bothIt
+    };
 
-    for (var i = 0; i < 100; i++) this.Things.push(i);
+    for (var i = 0; i < 100; i++) vm.Things.push(i);
+
+    function okIt() {
+        vm.Check1.Ok = true;
+        vm.Check1.Bad = false;
+    }
+
+    function badIt() {
+        vm.Check1.Ok = false;
+        vm.Check1.Bad = true;
+    }
+
+    function cancelIt() {
+        vm.Check1.Ok = false;
+        vm.Check1.Bad = false;
+    }
+
+    function bothIt() {
+        vm.Check1.Ok = true;
+        vm.Check1.Bad = true;
+    }
 }
---
+++ directives/d-check-glyph/d-check-glyph.directive.js
@@ -0,0 +1,61 @@
+﻿/**
+ * Flag-based Bootstrap glyphicon display for several attribute-specified 
+ * states:
+ * 
+ *   OK: Shows a green checkmark
+ *   Bad: Shows a red exclamation
+ *   Neither: Blank
+ *   Both: Some combination of the above; wily; avoid this.
+ * 
+ * Example:
+ * 
+ *   <d-check-glyph is-ok="main.Check1.Ok" is-bad="main.Check1.Bad" />
+ */
+"use strict";
+
+angular.module("app").directive("dCheckGlyph", dCheckGlyph);
+
+function dCheckGlyph() {
+    var OK_CLASS = "glyphicon-ok";
+    var OK_COLOR = "#0d0";
+
+    var BAD_CLASS = "glyphicon-exclamation-sign";
+    var BAD_COLOR = "#d00";
+
+    var scope = {
+        IsOk: "=isOk",
+        IsBad: "=isBad"
+    }
+
+    var directive = {
+        restrict: "E",
+        scope: scope,
+        link: link,
+        template: "<span class='glyphicon' ng-class='icon'></span>"
+    };
+
+    return directive;
+
+    function link(scope, element) {
+        scope.icon = {
+            OK_CLASS: scope.IsOk,
+            BAD_CLASS: scope.IsBad
+        };
+
+        scope.$watch("IsOk", function (val) {
+            scope.icon[OK_CLASS] = val;
+
+            if (val) setColor(OK_COLOR);
+        });
+
+        scope.$watch("IsBad", function (val) {
+            scope.icon[BAD_CLASS] = val;
+
+            if (val) setColor(BAD_COLOR);
+        });
+
+        function setColor(color) {
+            element[0].style.color = color;
+        }
+    }
+}
