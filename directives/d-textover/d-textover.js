de9f5a2898c68b9941bdfca74730d22b7b77127b
ssmock <ssmock@gmail.com>
2015-03-01 11:18:07 -0500

    initial

---
+++ directives/d-textover/d-textover.js
@@ -0,0 +1,66 @@
+﻿/**
+ * Provides content overlaying a simple text input; useful for providing 
+ * instructions and verification text, especially in the absence the 
+ * placeholder attribute.
+ * 
+ * To style properly, place inside of a div.d-textover-box.  Margin and padding
+ * values are set to work with Bootstrap's form-control classes.
+ * 
+ * Example:
+ * 
+ * <div class="d-textover-box">
+ *     <input type="text" id="thing" class="form-control"
+ *             ng-model="main.Input1" />
+ *     <d-textover left hide-with-input>
+ *         LEFT: <span ng-bind="main.Left"></span>
+ *     </d-textover>
+ *     <d-textover right>
+ *         RIGHT: <span ng-bind="main.Right"></span>
+ *     </d-textover>
+ * </div>
+ */
+"use strict";
+
+angular.module("app", []).directive("dTextover", dTextover);
+
+dTextover.$inject = ["$timeout"];
+
+function dTextover($timeout) {
+    var directive = {
+        restrict: "E",
+        template: "<div ng-show='shown' ng-class='class'><ng-transclude></ng-transclude></div>",
+        transclude: true,
+        scope: true,
+        link: link
+    };
+
+    return directive;
+
+    function link(scope, element, attributes, controller) {
+        scope.class =
+            attributes.hasOwnProperty("right")
+            ? "d-over-right"
+            : "d-over-left";
+
+        scope.shown = true;
+
+        if (attributes.hasOwnProperty("hideWithInput")) {
+            var input = element.parent().find("input");
+
+            input.on("keydown paste input", updateShow);
+
+            function updateShow() {
+                if (input.val()) {
+                    scope.shown = false;
+                }
+                else {
+                    scope.shown = true;
+                }
+
+                scope.$apply();
+            }
+
+            $timeout(updateShow, 0);
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module("app", []);
+
+app.controller("main", main);
+
+function main() {
+    var vm = this;
+
+    this.Left = "here";
+    this.Right = "there";
+    this.Input1 = "";
+    this.Things = [];
+
+    for (var i = 0; i < 100; i++) this.Things.push(i);
+}
